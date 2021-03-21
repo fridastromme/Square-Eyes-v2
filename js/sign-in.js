81d587ca872a295871a2aa27af75fa1558f0ede3
Fridastromme <fridastromme@hotmail.com>
2021-03-21 17:41:20 +0100

    Subtle highlight on My movies-section on producer profile

--- js/sign-in.js
+++ js/sign-in.js
@@ -4,8 +4,10 @@ const passwordError = document.querySelector("#passwordError");
 const button = document.querySelector(".submit-2");
 
 function validateForm(event) {
+
 	event.preventDefault();
-	if ((validateEmail(email.value)) && (validatePassword(password.value))) {
+    
+	if ((validateEmail(email.value))) {
 		window.location.href = "producer-profile.html";
 	} else {
 		passwordError.style.display = "block";
@@ -18,10 +20,4 @@ function validateEmail(email) {
 	const regEx = /\S+@\S+\.\S+/;
 	const emailMatches = regEx.test(email);
 	return emailMatches;
-}
-
-function validatePassword(password) {
-	const regEx = /(?=(.*[0-9]))((?=.*[A-Za-z0-9])(?=.*[A-Z])(?=.*[a-z]))^.{8,}$/;
-	const passwordMatches = regEx.test(password);
-	return passwordMatches;
 }
